0d6d3a0b69caeffada8ab6f0bac124103850b562
Mark <markmyskovets9@gmail.com>
2025-03-30 20:25:22 +0300

    13hw.cpp

--- 13_FormatFunctions/13hw.cpp
+++ 13_FormatFunctions/13hw.cpp
@@ -6,7 +6,8 @@ int main()
 	//1
 	const int size = 255;
 	int counter = 0, count_a = 0, count_o = 0, count_al = 0, count_num = 0, count_space = 0;
-	char massive[] = {"Lorem an ipsum dolor sit 2 times to amet"}, user_massive[size];
+	char massive[] = {"Lorem an ipsum dolor sit 2 times to amet"};
+
 	for (int i = 0; i < sizeof(massive); i++)
 	{
 		cout << massive[i];
@@ -61,15 +62,74 @@ int main()
 	}
 
 	//4
-	cout << "Enter your line:\n";
-	cin.getline(user_massive, 255);
-	for (int i = 0; i < size; i++)
+	
+	/*char userString[100]; 
+	int length = 0;
+	char* ptr = userString;
+
+	cout << "Enter a string: ";
+	cin.getline(userString, 100);
+
+	while (*(ptr + length) != '\0') 
 	{
-		counter++;
+		length++;
 	}
-	cout << "Len of your line is " << counter << endl;
-	cout << endl;
+
+	cout << "Length of the string: " << length << "\n";*/
 
 	//5
+
+	/*char simbol;
+	cout << "\n" << "Enter a simbol you want to delete: "; cin >> simbol;
+	for (int i = 0; i < sizeof(massive); i++)
+	{
+		if (massive[i] == simbol) {
+			continue;
+		}
+		cout << massive[i];
+	}*/
+
+	//6
+	char userString1[100];
+	char vowels[] = { "aeiouAEIOU" };
+	char consonants[] = { "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz" };
+	char punct[] = { "!'#$%&'()*+,-./:;<=>?@[]^_`{|}~" };
+	int countv = 0, countc = 0, countp = 0, counts = 0;
+	cout << "Enter a string: ";
+	cin.getline(userString1, 100);
+
+	for (int i = 0; i < sizeof(userString1)-2; i++)
+	{
+		for (int j = 0; j < sizeof(vowels); j++)
+		{
+			if (userString1[i] == vowels[j])
+			{
+				countv++;
+				continue;
+			}
+		}
+		for (int j = 0; j < sizeof(consonants); j++)
+		{
+			if (userString1[i] == consonants[j])
+			{
+				countc++;
+				continue;
+			}
+		}
+		for (int j = 0; j < sizeof(punct); j++)
+		{
+			if (userString1[i] == punct[j])
+			{
+				countp++;
+				continue;
+			}
+		}
+		if (userString1[i] == ' ')
+		{
+			counts++;
+		}
+	}
+
+	cout << "Amount of...\nVowels = " << countv-1 << "\nConsonants = " << countc-1 << "\nPunctuation marks = " << countp << "\nSpaces = " << counts << "\n";
 
 }
