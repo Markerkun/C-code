f56ba08671e8741c9d95e062230f7c85cffa51d7
Mark <markmyskovets9@gmail.com>
2025-04-09 17:06:59 +0300

    18pw );

--- 18_UnionFiles/18_UnionFiles.cpp
+++ 18_UnionFiles/18_UnionFiles.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <string
 
 using namespace std;
 
 int main()
 {
 
-	ofstream fout;
-	ifstream fin;
-	fout.open("Text2.txt");
-	fin.open("Text1.txt");
+	ifstream fin1;
+	ifstream fin2;
+	fin1.open("Text1.txt");
 
-	char buffer[200];
-	fin.getline(buffer, 196);
+	char text[] = { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." };
+	fin1.getline(text, 125);
 	
-	for (int i = 0; i < 194; i++)
-	{
-		fout << buffer[i];
-	}
+	
+	fin2.open("Text2.txt");
+	fin2.getline(text, 123);
+
+		
 
 
-	fout.close();
-	fin.close();	
+
+    char line1[124];
+    char line2[124];
+    int indx = 0;
+    bool flag = true;
+    line1.getline(fin1, 124);
+    while (line1[indx] == line2[indx])
+    {
+        if (line1[indx] != line2[indx]) 
+        {
+            cout << "Elem on index " << indx << " isn't match\n";
+            cout << "file 1: " << line1 << "\n";
+            cout << "file 2: " << line2 << "\n";
+            flag = false;
+        }
+        indx++;
+    }
+    
+
+    fin1.close();
+    fin2.close();
 
 
 
