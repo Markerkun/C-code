2dc4dcc4d62724d8c86ac4fe9cad8a47e9c05c5f
DEV <DEV@A21PC16>
2025-04-16 20:55:35 +0300

    24pw

--- 24_ConstExplisit/24_ConstExplisit.cpp
+++ 24_ConstExplisit/24_ConstExplisit.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void doSomething(long number)
+void bubbleSortPlus(long arr[], int size)
 {
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1]) 
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
-float doSomething()
+void bubbleSortMinus(long arr[], int size)
 {
-	return 10.0;
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
 
 class Array
@@ -46,7 +69,7 @@ public:
 	{
 		return size;
 	}
-	int getValue(int index)const
+	long getValue(int index)const
 	{
 		return arr[index];
 	}
@@ -58,10 +81,127 @@ public:
 	{
 		cout << arr[index] << " ";
 	}
+	void display()const
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+	void randomValue()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 100;
+		}
+	}
+	void AddValue(long value)
+	{
+		long* temp = new long[size + 1];
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = arr[i];
+		}
+		temp[size] = value;
+		delete[]arr;
+		arr = temp;
+		size++;
+	}
+	void DeleteByValue(int value)
+	{
+		long* temp = new long[size - 1];
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i] == value)
+			{
+				i--;
+				continue;
+			}
+			temp[i] = arr[i];
+		}
+		delete[]arr;
+		arr = temp;
+		size--;
+	}
+	void DeleteByIndex(int index)
+	{
+		long* temp = new long[size - 1];
+		for (int i = 0; i < size; i++)
+		{
+			if (i == index)
+			{
+				i--;
+				continue;
+			}
+			temp[i] = arr[i];
+		}
+		delete[]arr;
+		arr = temp;
+		size--;
+	}
+	void SortPlus()
+	{
+		bubbleSortPlus(arr, size);
+	}
+	void SortMinus()
+	{
+		bubbleSortMinus(arr, size);
+	}
+	long Max() const
+	{
+		long max = arr[0];
+		for (int i = 1; i < size; i++)
+		{
+			if (arr[i] > max)
+			{
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+	long Min() const
+	{
+		long min = arr[0];
+		for (int i = 1; i < size; i++)
+		{
+			if (arr[i] < min)
+			{
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+	double Average() const
+	{
+		double sum = 0;
+		for (int i = 0; i < size; i++)
+		{
+			sum += arr[i];
+		}
+		return sum / size;
+	}
 };
 
 
 void main()
 {
+	Array arr(5);
+	arr.display();
+	arr.randomValue();
+	arr.display();
+	arr.AddValue(100);
+	arr.display();
+	arr.DeleteByValue(0);
+	arr.display();
+	arr.DeleteByIndex(0);
+	arr.display();
+	arr.SortPlus();
+	arr.display();
+	arr.SortMinus();
+	arr.display();
+	cout << "Max: " << arr.Max() << endl;
+	cout << "Min: " << arr.Min() << endl;
+	cout << "Average: " << arr.Average() << endl;
 
 }
