16b7ddd0a68fd1ca0ea4ffc87617526f698d9fbb
Mark <markmyskovets9@gmail.com>
2025-05-19 16:44:50 +0300

    28 task 2

--- 28_InitializerListTemplateClass/28_InitializerListTemplateClass.cpp
+++ 28_InitializerListTemplateClass/28_InitializerListTemplateClass.cpp
@@ -49,6 +49,82 @@ public:
 		}
 		cout << endl;
 	}
+
+	void Max()
+	{
+		T max = arr[0];
+		for (int i = 1; i < size; i++)
+		{
+			if (arr[i] > max)
+				max = arr[i];
+		}
+		cout << "Max: " << max << endl;
+	}
+
+	void Min()
+	{
+		T min = arr[0];
+		for (int i = 1; i < size; i++)
+		{
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		cout << "Min: " << min << endl;
+	}
+
+	void Sort()
+	{
+		for (int i = 0; i < size - 1; i++)
+		{
+			for (int j = 0; j < size - i - 1; j++)
+			{
+				if (arr[j] > arr[j + 1])
+				{
+					T temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	T Find(T key)
+	{
+		Sort();
+		int start = 0;
+		int end = size - 1;
+		while (start <= end)
+		{
+			int mid = start + (end - start) / 2;
+			if (arr[mid] == key)
+			{
+				return mid;
+			}
+			else if (arr[mid] < key) {
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+	void replace(int index, T value)
+	{
+		if (index >= 0 && index < size)
+		{
+			arr[index] = value;
+		}
+		else
+		{
+			cout << "Index out of range" << endl;
+		}
+	}
+
+
+
 	~Array()
 	{
 		if (arr != nullptr)
